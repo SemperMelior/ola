121d3a9c5805bbdb8e63d449fd66aa262ac2101c
Justin Womersley <justinwomersley@gmail.com>
2015-02-04 14:13:42 -0800

    Small changes

--- Ola/AddFoodTVC.m
+++ Ola/AddFoodTVC.m
@@ -75,7 +75,6 @@
     return cell;
 }
 
-
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
