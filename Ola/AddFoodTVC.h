e5063e0a53a7a2a5b81e99fce6c8abb0b69c1b1c
Justin Womersley <justinwomersley@gmail.com>
2015-02-04 12:08:14 -0800

    Added food database and new food search

---
+++ Ola/AddFoodTVC.h
@@ -0,0 +1,13 @@
+//
+//  AddFoodTVC.h
+//  Ola
+//
+//  Created by Justin Womersley on 2/4/15.
+//  Copyright (c) 2015 cs147. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFoodTVC : UITableViewController
+
+@end
---
+++ Ola/CarbCountView.m
@@ -0,0 +1,21 @@
+//
+//  CarbCountView.m
+//  Ola
+//
+//  Created by Justin Womersley on 2/4/15.
+//  Copyright (c) 2015 cs147. All rights reserved.
+//
+
+#import "CarbCountView.h"
+
+@implementation CarbCountView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
